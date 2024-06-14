8effc431b027202605ee57c52887de1ba1a28200
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-06-14 13:45:05 +0200

    chore: Center text

--- src/scenes/Testimonials.jsx
+++ src/scenes/Testimonials.jsx
@@ -9,7 +9,7 @@ const Testimonials = () => {
     <section id="témoignages" className="pt-32 pb-16">
       {/* HEADING */}
       <motion.div
-        className="md:w-1/3 text-center md:text-left"
+        className="md:w-1/3 text-center md:text-center"
         initial="hidden"
         whileInView="visible"
         viewport={{ once: true, amount: 0.5 }}
